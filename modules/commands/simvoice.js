2fce14ee55436dce2ca107b1982641951d3161bd
Nguyễn Chí Thành Lộc <96804285+GinzaTech@users.noreply.github.com>
2022-02-05 08:35:10 +0700

    Update and rename simrepvoice.js to simvoice.js

--- modules/commands/simvoice.js
+++ modules/commands/simvoice.js
@@ -4,7 +4,7 @@
  * @apikey Reg key tại: https://meewmeew.info/site
  */
 module.exports.config = {
-    name: "sim",
+    name: "simvoice",
     version: "4.3.6",
     hasPermssion: 0,
     credits: "ProCoderMew",
